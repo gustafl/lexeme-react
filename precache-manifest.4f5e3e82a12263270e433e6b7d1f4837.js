6c2b0f2ada84fb394efda94f7226c132dd3dcd6f
Gustaf Liljegren <gustafl@bahnhof.se>
2019-03-27 18:34:15 +0100

    Updates

---
+++ precache-manifest.4f5e3e82a12263270e433e6b7d1f4837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91765840dcd86e2df7c6",
+    "url": "/lexeme-react/static/js/runtime~main.91765840.js"
+  },
+  {
+    "revision": "f3ff83be937d7a8fce1e",
+    "url": "/lexeme-react/static/js/main.f3ff83be.chunk.js"
+  },
+  {
+    "revision": "e330cb724c0a43195191",
+    "url": "/lexeme-react/static/js/2.e330cb72.chunk.js"
+  },
+  {
+    "revision": "f3ff83be937d7a8fce1e",
+    "url": "/lexeme-react/static/css/main.31234d31.chunk.css"
+  },
+  {
+    "revision": "4651350978eefd6753bda415e4c40cb2",
+    "url": "/lexeme-react/index.html"
+  }
+];
