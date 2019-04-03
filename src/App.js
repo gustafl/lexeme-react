3b4d1f376bb1db3b56e54deb33a26d6ccd4b97bb
Gustaf Liljegren <gustafl@bahnhof.se>
2019-04-03 22:48:22 +0200

    Added HtmlContent component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HtmlContent from './HtmlContent';
 import Form from './Form';
 import './App.scss';
 
@@ -8,9 +9,7 @@ class App extends Component {
       <div className="wrapper">
         <header className="header">Header</header>
         <article className="main">
-          <h2>1. Un écueil fuyant</h2>
-          <p>L'année 1866 fut marquée par un événement <span className="adjective">bizarre</span>, un phénomène inexpliqué et inexplicable que personne n'a sans doute oublié. Sans parler des rumeurs qui agitaient les populations des ports et surexcitaient l'esprit public à l'intérieur des continents les gens de mer furent particulièrement émus. Les négociants, armateurs, capitaines de navires, skippers et masters de l'Europe et de l'Amérique, officiers des marines militaires de tous pays, et, après eux, les gouvernements des divers États des deux continents, se préoccupèrent de ce fait au plus haut point.</p>
-          <p>En effet, depuis quelque temps, plusieurs navires s'étaient rencontrés sur mer avec « une chose énorme » un objet long, fusiforme, parfois phosphorescent, infiniment plus vaste et plus rapide qu'une baleine.</p>
+          <HtmlContent />
         </article>
         <aside className="form">
           <Form/>
---
+++ src/HtmlContent.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import './HtmlContent.scss';
+
+class HtmlContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            html: { __html: '<h1>Solen är rund</h1><p>En text som är bra. En krokodil äter &lt; 9 äpplen om dagen.</p>' },
+        };
+        this.isValidLetter = this.isValidLetter.bind(this);
+        this.selectionHandler = this.selectionHandler.bind(this);
+    }
+
+    isValidLetter(character) {
+        character = character.replace(/[^a-zA-Z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF]/, '');
+        return (character !== '');
+    }
+
+    selectionHandler() {
+        
+        const SELECTION_MAX_LENGTH = 100;
+    
+        // Get selection object
+        let selection = window.getSelection();
+    
+        // Make sure anchor and focus nodes are the same
+        if (selection.anchorNode !== selection.focusNode) {
+            console.warn('The anchor and focus nodes are different.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Get anchor node object
+        let node = selection.anchorNode;
+
+        // Make sure the parent of this textnode is not a <span>
+        if (node.parentNode.nodeName === 'SPAN') {
+            console.warn('There is already a highlight here.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Make sure it's a text node
+        if (node.nodeType !== 3) {
+            console.warn('The node is not a text node.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Get start and end of selection
+        let start = selection.anchorOffset;
+        let end = selection.focusOffset;
+
+        // Make sure we got a proper mousedown-drag-mouseup selection
+        if (start === end) {
+            console.warn('At least one character must be selected.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Reverse start and end if user made a backwards selection
+        if (start > end) {
+            let temp = end;
+            end = start;
+            start = temp;
+        }
+
+        // Make sure selection is not too long
+        if ((end - start) > SELECTION_MAX_LENGTH) {
+            console.warn('The selection is too long.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Get selected text
+        let selectedText = node.nodeValue.substring(start, end);
+
+        // Make sure all characters in selection are valid letters or spaces
+        let numberOfValidLetters = 0;
+        for (let i = 0; i < selectedText.length; i++) {
+            if (this.isValidLetter(selectedText[i]) || selectedText[i] === ' ') {
+                if (selectedText[i] !== ' ') {
+                    numberOfValidLetters++;
+                }
+            } else {
+                console.warn('The selection contains invalid characters.');
+                selection.removeAllRanges();
+                return;
+            }
+        }
+
+        // Move selection start right until the first non-space character
+        while (start >= 0 && node.nodeValue.substring(start, start + 1) === ' ') {
+            start++;
+        }
+
+        // Move selection end left until the first non-space character
+        while (end <= node.nodeValue.length && node.nodeValue.substring(end - 1, end) === ' ') {
+            end--;
+        }
+
+        // Move selection start left while there are valid letters
+        while (start >= 0 && this.isValidLetter(node.nodeValue.substring(start - 1, start))) {
+            start--;
+        }
+
+        // Move selection end right while there are valid letters
+        while (end <= node.nodeValue.length - 1 && this.isValidLetter(node.nodeValue.substring(end, end + 1))) {
+            end++;
+        }
+
+        // Adjust selection
+        selection.collapse(node, start);
+        selection.extend(node, end);
+
+        // Make sure selection has at least one valid letter
+        if (numberOfValidLetters === 0) {
+            console.warn('The selection must have at least one valid letter.');
+            selection.removeAllRanges();
+            return;
+        }
+
+        // Get adjusted selected text
+        var adjustedSelectedText = node.nodeValue.substring(start, end);
+        console.info('A selection was made: %s (%d:%d)', adjustedSelectedText, start, end);
+
+        // Remember this selection (in case we need to restore it)
+        //window.lastSelection.node = node;
+        //window.lastSelection.start = start;
+        //window.lastSelection.end = end;
+
+        // Adjust form according to selection
+        //changeWord(adjustedSelectedText);
+    }
+
+    render() {
+        return (
+            <div contentEditable="true" dangerouslySetInnerHTML={this.state.html} onMouseUp={this.selectionHandler}/>
+        );
+    }
+}
+
+export default HtmlContent;
